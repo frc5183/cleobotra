c9273e8a7e2b031bd02c11b6ae2ab7c3bf989ad1
Bacon <iam@baconi.ng>
2026-03-23 16:22:49 -0400

    chore(climber): remove safety limits
    our operator is confident enough and its usually only active occasionally

--- src/main/kotlin/org/frc5183/robot/constants/Constants.kt
+++
@@ -1,9 +0,0 @@
-package org.frc5183.robot.constants
-
-import edu.wpi.first.units.Units
-import edu.wpi.first.units.measure.Angle
-
-object Constants {
-
-    val CLIMBER_MAX: Angle = Units.Rotations.of(9999.0)
-}
