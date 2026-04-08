bf19eb7b53789a0437046c0e078de2992f546364
Bacon <iam@baconi.ng>
2026-04-08 15:40:14 -0400

    feat(shooter): add shooting by distance

--- src/main/kotlin/org/frc5183/robot/subsystems/shooter/ShooterSubsystem.kt
+++ src/main/kotlin/org/frc5183/robot/subsystems/shooter/ShooterSubsystem.kt
@@ -38,6 +38,12 @@ class ShooterSubsystem(
         runFeeder(-1.0)
     }
 
+    fun shoot(velocity: AngularVelocity) {
+        runShooterVelocity(velocity)
+        runIntake(-1.0)
+        runFeeder(-1.0)
+    }
+
     fun runShooter(speed: Double) = shooter.set(-speed)
     fun runShooterVelocity(velocity: AngularVelocity) = shooter.closedLoopController.setSetpoint(velocity.`in`(Units.RPM), SparkBase.ControlType.kVelocity)
     fun runIntake(speed: Double) = intake.set(-speed)
